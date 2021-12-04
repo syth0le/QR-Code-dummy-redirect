7c0d45656804e2e8aaf63088adf4324af2f21102
syth0le <chdan565@gmail.com>
2021-12-04 16:30:36 +0300

    add endpoints and run.py file

--- app/server.py
+++ app/server.py
@@ -1,17 +1,29 @@
-from flask import Flask
+import qrcode
+from flask import Flask, send_file
+from qrcode.image.styles.moduledrawers import SquareModuleDrawer
 
 app = Flask('QR code redirecter')
 
 
 @app.get('/generate')
 def generate_qr_code():
-    return 'QR code redirecter'
+    qr = qrcode.QRCode(
+        version=3,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=20,
+        border=4,
+    )
+    # qr.add_data(url_for('get_dummy_request_from_qr_code'))
+    qr.add_data('edu.vsu.ru')
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="black",
+                          back_color="white",
+                          module_drawer=SquareModuleDrawer())
+    image.save('qrcode.jpeg')
+    return send_file('qrcode.jpeg', mimetype='image/gif')
 
 
 @app.get('/dummy_request')
 def get_dummy_request_from_qr_code():
     return 'Picture!'
-
-
-if __name__ == '__main__':
-    app.run('127.0.0.1', 8080, debug=True)
