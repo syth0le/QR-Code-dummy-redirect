58a554f171eecc9de31692bd5c83399d9b157b5d
syth0le <chdan565@gmail.com>
2021-12-05 00:18:01 +0300

    update gitignore and server.py rewrite responses

--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
 import qrcode
-from flask import Flask, send_file
+from flask import Flask, render_template, send_file
 from qrcode.image.styles.moduledrawers import SquareModuleDrawer
 
 app = Flask('QR code redirecter')
@@ -8,7 +8,7 @@ app = Flask('QR code redirecter')
 @app.get('/generate')
 def generate_qr_code():
     qr = qrcode.QRCode(
-        version=3,
+        version=6,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=20,
         border=4,
@@ -20,10 +20,10 @@ def generate_qr_code():
     image = qr.make_image(fill_color="black",
                           back_color="white",
                           module_drawer=SquareModuleDrawer())
-    image.save('qrcode.jpeg')
-    return send_file('qrcode.jpeg', mimetype='image/gif')
+    image.save('./static/images/qrcode.jpeg')
+    return send_file('./static/images/qrcode.jpeg', mimetype='image/gif')
 
 
 @app.get('/dummy_request')
 def get_dummy_request_from_qr_code():
-    return 'Picture!'
+    return render_template('index.html')
