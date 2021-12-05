b01029eb7e1b2348856a443645df7f903cbaf25a
syth0le <chdan565@gmail.com>
2021-12-05 13:31:30 +0300

    add new packages to requirements.txt and add config

---
+++ app/config.py
@@ -0,0 +1,2 @@
+QR_CODE_PATH = './static/images/qrcode.jpeg'
+SITE_URL = 'adidas.ru'
--- app/server.py
+++ app/server.py
@@ -1,27 +1,15 @@
-import qrcode
 from flask import Flask, render_template, send_file
-from qrcode.image.styles.moduledrawers import SquareModuleDrawer
+
+from app.config import QR_CODE_PATH, SITE_URL
+from app.utils import generator_qr_code
 
 app = Flask('QR code redirecter')
 
 
 @app.get('/generate')
-def generate_qr_code():
-    qr = qrcode.QRCode(
-        version=6,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=20,
-        border=4,
-    )
-    # qr.add_data(url_for('get_dummy_request_from_qr_code'))
-    qr.add_data('edu.vsu.ru')
-    qr.make(fit=True)
-
-    image = qr.make_image(fill_color="black",
-                          back_color="white",
-                          module_drawer=SquareModuleDrawer())
-    image.save('./static/images/qrcode.jpeg')
-    return send_file('./static/images/qrcode.jpeg', mimetype='image/gif')
+def get_generated_qr_code():
+    generator_qr_code(qr_path=QR_CODE_PATH, site_domain=SITE_URL)
+    return send_file(QR_CODE_PATH, mimetype='image/gif')
 
 
 @app.get('/dummy_request')
