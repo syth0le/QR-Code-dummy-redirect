4d8abe7c0e498b565bb06918ab520dddda6e46ac
syth0le <chdan565@gmail.com>
2021-12-05 17:31:28 +0300

    rewrite get file from os functionality

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname, realpath
 
-QR_CODE_PATH = join(dirname(realpath(__file__)), './static/images/qrcode.jpeg')
+QR_CODE_PATH = join(dirname(realpath(__file__)), 'qrcode.jpeg')
 TEMPLATE_FOLDER = join(dirname(realpath(__file__)), 'templates')
 SITE_URL = 'adidas.ru'
