847c84f72883f36188916923cd405c9681c738f8
syth0le <chdan565@gmail.com>
2021-12-05 13:31:46 +0300

    add test for utils

---
+++ app/test/test_utils.py
@@ -0,0 +1,20 @@
+import os
+
+import pytest
+
+from app.config import QR_CODE_PATH
+from app.utils import generator_qr_code
+
+
+@pytest.fixture
+def mocked_url_for(mocker):
+    return mocker.patch('app.utils.get_full_site_url')
+
+
+def test_generator_qr_code(mocked_url_for):
+    mocked_url_for.return_value = '127.0.0.1:8080/dummy_request'
+
+    result_file = 'app' + QR_CODE_PATH[1:]
+    generator_qr_code(qr_path=result_file, site_domain='127.0.0.1:8080')
+
+    assert os.path.exists(result_file)
